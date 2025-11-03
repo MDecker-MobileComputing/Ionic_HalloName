cb0b6460c47578d70d9e76a3f2c1d11ae137f6c7
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-11-03 15:45:52 +0100

    Aktualisiert auf: @ionic/angular 8.7.8

--- android/app/src/main/java/de/mide/ionic/halloname/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package de.mide.ionic.halloname;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AlertController, ToastController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
+  standalone: false
 })
 export class HomePage {
 
